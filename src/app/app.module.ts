4fa2e044f9b8b965dd240afb8afd0001bee43700
Leandro Alves <lalvesevangelista@gmail.com>
2023-08-11 18:33:37 -0300

    Adicionado Angular Material e componentizando detalhes do heroi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,44 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Endevor',
+  },
+  {
+    id: 2,
+    name: 'Hawks',
+  },
+  {
+    id: 3,
+    name: 'Best Jeanist',
+  },
+  {
+    id: 4,
+    name: 'Edgeshot',
+  },
+  {
+    id: 5,
+    name: 'Mirko',
+  },
+  {
+    id: 6,
+    name: 'Kamui Woods',
+  },
+  {
+    id: 7,
+    name: 'Wash',
+  },
+  {
+    id: 8,
+    name: 'Ryukyu',
+  },
+  {
+    id: 9,
+    name: 'Gang Orca',
+  },
+  {
+    id: 10,
+    name: 'Shishido',
+  },
+];
