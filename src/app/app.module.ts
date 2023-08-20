a91722f3d7967cb4d1687b87696b2dac43d94cf6
Leandro Alves <lalvesevangelista@gmail.com>
2023-08-20 19:23:36 -0300

    Reorganizacao de arquivos por features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
+    // @angular
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    // feature
+    DashboardModule,
+    HeroesModule,
+    // app
     CoreModule,
     AppRoutingModule,
   ],
