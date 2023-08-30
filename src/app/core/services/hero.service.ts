606987be0d0cd92af4c4db2cef8968b3a9c858dc
Leandro Alves <lalvesevangelista@gmail.com>
2023-08-29 23:14:44 -0300

    feat: editar e criar heroi

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -19,7 +19,7 @@ export class HeroService {
   ) {}
 
   // GET /heroes
-  getHeroes(): Observable<Hero[]> {
+  getAll(): Observable<Hero[]> {
     this.loading = true;
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap((heroes) =>
@@ -31,13 +31,27 @@ export class HeroService {
     );
   }
 
+  // POST /heroes
+  create(hero: Hero): Observable<Hero> {
+    return this.http
+      .post<Hero>(this.heroesUrl, hero)
+      .pipe(tap((hero) => this.log(`fetched ${this.descAttributes(hero)}`)));
+  }
   // GET /heroes/id
-  getHero(id: number): Observable<Hero> {
+  getOne(id: number): Observable<Hero> {
     return this.http
       .get<Hero>(`${this.heroesUrl}/${id}`)
-      .pipe(
-        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
-      );
+      .pipe(tap((hero) => this.log(`fetched ${this.descAttributes(hero)}`)));
+  }
+  // PUT /heroes/id
+  update(hero: Hero): Observable<Hero> {
+    return this.http
+      .put<Hero>(`${this.heroesUrl}/${hero.id}`, hero)
+      .pipe(tap((hero) => this.log(`updated ${this.descAttributes(hero)}`)));
+  }
+
+  private descAttributes(hero: Hero): string {
+    return `Hero ID=${hero.id} and Name=${hero.name}`;
   }
 
   private log(message: string): void {
