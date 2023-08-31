b1625b4a690ecdb86fcbc59b7e96c6139a028486
Leandro Alves <lalvesevangelista@gmail.com>
2023-08-30 23:13:37 -0300

    feat: validacao de formulario e snackbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
